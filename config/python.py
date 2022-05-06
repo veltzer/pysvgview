68092f6d18b6a2ebb6e6a1a5dd77c4add9e23c08
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:12:24 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -3,39 +3,30 @@ import config.project
 package_name = config.project.project_name
 
 console_scripts = [
-    'pysvgview=pysvgview.main:main',
-]
-
-setup_requires = [
+    "pysvgview=pysvgview.main:main",
 ]
 
 run_requires = [
-    'PyQt5',
-    'pytconf',
-    'pylogconf',
+    "PyQt5",
+    "pytconf",
+    "pylogconf",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'flake8',
-    'pymakehelper',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "pymakehelper",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
-    'PyQt5-stubs',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
+    "PyQt5-stubs",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
