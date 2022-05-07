6fb46c49a258c995edbc2271262324ded4e882f5
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-07 05:06:45 +0300

    better templates

--- setup.py
+++ setup.py
@@ -32,13 +32,6 @@ setuptools.setup(
     platforms=[
         'python3',
     ],
-    install_requires=[
-        'PyQt5',
-        'pytconf',
-        'pylogconf',
-    ],
-    extras_require={
-    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
