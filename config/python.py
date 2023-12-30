9a70934b07781eb52805fa344f558ec3fcc7a6f1
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 02:53:43 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,25 +1,30 @@
-console_scripts = [
+from typing import List
+
+
+console_scripts: List[str] = [
     "pysvgview=pysvgview.main:main",
 ]
-dev_requires = [
+config_requires: List[str] = []
+dev_requires: List[str] = [
     "pypitools",
 ]
-make_requires = [
-    "pyclassifiers",
-    "pydmt",
-    "pymakehelper",
-]
-install_requires = [
+install_requires: List[str] = [
     # "PyQt6-stubs",
     "PyQt6",
     "PySide6",
     "pytconf",
     "pylogconf",
 ]
-test_requires = [
+make_requires: List[str] = [
+    "pyclassifiers",
+    "pydmt",
+    "pymakehelper",
+]
+test_requires: List[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
     "flake8",
     "mypy",
 ]
+requires = config_requires + install_requires + make_requires + test_requires
