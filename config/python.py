deff50ed2134a3cd55b01e1072c4a5adbbfd4943
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:17 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,12 +1,7 @@
 """ python deps for this project """
 
-scripts: dict[str,str] = {
-    "pysvgview": "pysvgview.main:main",
-}
+import config.shared
 
-config_requires: list[str] = [
-    "pyclassifiers",
-]
 install_requires: list[str] = [
     # "PyQt6-stubs",
     "PyQt6",
@@ -14,16 +9,10 @@ install_requires: list[str] = [
     "pytconf",
     "pylogconf",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-]
-requires = config_requires + install_requires + build_requires + test_requires
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+requires = install_requires + build_requires + test_requires
+
+scripts: dict[str,str] = {
+    "pysvgview": "pysvgview.main:main",
+}
